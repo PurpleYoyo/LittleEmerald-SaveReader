8a8e24ceb9cc1392674340d9ba636819e3571941
Purkat <43706820+PurpleYoyo@users.noreply.github.com>
2026-03-26 17:36:37 -0400

    d

--- js/main.js
+++ js/main.js
@@ -65,4 +65,4 @@ document.getElementById('search-bar').addEventListener('input', function () {
 
 document.getElementById('pokemon').addEventListener('input', () => pokedex.renderCards(data.pokemonData));
 document.getElementById('moves').addEventListener('input', () => moves.renderCards(data.moveData));
-document.getElementById('locations').addEventListener('input', () => locations.renderMap(data.locationData));
+//document.getElementById('locations').addEventListener('input', () => locations.renderMap(data.locationData));
