b87dcbb1f3f4d0192caa2869daa72d7181bf3be0
Purkat <43706820+PurpleYoyo@users.noreply.github.com>
2026-04-04 22:05:38 -0400

    f

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,18 @@
 import { subareaMapping } from './data.js';
 
+function formatSubarea(name) {  
+    return name
+        .toLowerCase()
+        .replace(/^map_/, '')
+        .split('_')
+        .map(word => {
+            let formatted = word.charAt(0).toUpperCase() + word.slice(1);
+            formatted = formatted.replace(/(\d+)f$/i, (_, num) => num + 'F');
+            return formatted;
+        })
+        .join(' ');
+}
+
 export function title(name) {  
     return name
         .split('-')
@@ -8,16 +21,19 @@ export function title(name) {
 }
 
 export function format(str, ...args) {
-    return str.replace(/{(\d+)}/g, (match, index) => args[index] ?? match)
+    return str.replace(/{(\d+)}/g,
+        (match, index) => args[index] ?? match
+    );
 }
 
 export function buildFormattedArray(original, link, data) {
     if (!original) return ['None'];
+    console.log(original);
+    console.log(!original);
 
     return (original).map(val => {
         const sub = subareaMapping[val] ?? val;
         const name = data[sub]?.name ?? 'None';
-        const new_link = sub === val ? format(link, name) : format(link, name, sub)
-        return name == 'None' ? 'None' : new_link
+        return sub === val ? format(link, name) : format(link, name, formatSubarea(val));
     });
 }
